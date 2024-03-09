347b3b5addedddfcdf25750e691d108b753e1c60
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 15:58:37 -0300

    feature: adiciona controller da classe de atividade
    O controller possui o método de POST e o método GET por id

---
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Controllers/AtividadeController.cs
@@ -0,0 +1,35 @@
+﻿using api_gerenciador_de_atividades.Data;
+using api_gerenciador_de_atividades.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_gerenciador_de_atividades.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class AtividadeController : ControllerBase
+{
+    private AtividadeContext _contexto;
+
+    public AtividadeController(AtividadeContext contexto)
+    {
+        _contexto = contexto;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionaAtividade([FromBody] Atividade atividade)
+    {
+        _contexto.Atividades.Add(atividade);
+        _contexto.SaveChanges();
+        return CreatedAtAction(nameof(RecuperaAtividadesPorId), new { id = atividade.Id }, atividade);
+    }
+
+    [HttpGet("{id}")]
+    private IActionResult RecuperaAtividadesPorId(int id)
+    {
+        Atividade atividade = _contexto.Atividades.Find(id);
+        if (atividade != null)
+        {
+            return Ok(atividade);
+        }
+        return NotFound();
+    }
+}
