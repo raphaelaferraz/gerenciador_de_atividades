abb1c827345aeeddb39a28c4c2984df46962a4ca
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 20:36:42 -0300

    refactor: adiciona comentários na classe de contexto de banco de dados de atividade

--- backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/AtividadeContext.cs
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/AtividadeContext.cs
@@ -3,6 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api_gerenciador_de_atividades.Data;
 
+/// <summary>
+/// Classe responsável por representar o contexto do banco de dados.
+/// </summary>
 public class AtividadeContext : DbContext
 {
     public AtividadeContext(DbContextOptions<AtividadeContext> options) : base(options)
