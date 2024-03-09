bbba1dc669d1bbe363c8cb239ce9fa322db913ec
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 20:00:12 -0300

    feature: adiciona DAO de atividade
    O DAO é responsável pelo acesso de informações no banco de dados

--- backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/Dao/AtividadeDao.cs
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/Dao/AtividadeDao.cs
@@ -1,6 +1,31 @@
-﻿namespace api_gerenciador_de_atividades.Data.Dao
+﻿using api_gerenciador_de_atividades.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_gerenciador_de_atividades.Data.Dao;
+
+public class AtividadeDao
 {
-    public class AtividadeDao
+    private readonly AtividadeContext _context;
+
+    public AtividadeDao(AtividadeContext context)
     {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Atividade>> GetAtividades()
+    {
+        return await _context.Atividades.ToListAsync();
+    }
+
+    public async Task<Atividade> GetAtividadeById(int id)
+    {
+        return await _context.Atividades.FirstOrDefaultAsync(atividade => atividade.Id == id);
+    }
+
+    public async Task<Atividade> AddAtividade(Atividade atividade)
+    {
+        _context.Atividades.Add(atividade);
+        await _context.SaveChangesAsync();
+        return atividade;
     }
 }
