027d5cee9ceeacb4f9021422f128ffda84701e09
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 15:22:37 -0300

    feature: adiciona classe de contexto do banco de dados

---
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/TarefaContext.cs
@@ -0,0 +1,12 @@
+﻿namespace api_gerenciador_de_atividades.Data;
+
+public class TarefaContext : DbContext
+{
+    public TarefaContext(DbContextOptions<TarefaContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Tarefa> Tarefas { get; set; }
+}
+{
+}
