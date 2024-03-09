57ea443b5eb003489a2bee95e939f55ccfabb6e2
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 20:42:32 -0300

    refactor: adiciona comentários na classe de DAO de atividade

--- backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/Dao/AtividadeDao.cs
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Data/Dao/AtividadeDao.cs
@@ -5,6 +5,9 @@ namespace api_gerenciador_de_atividades.Data.Dao;
 
 public class AtividadeDao
 {
+    /// <summary>
+    /// Atributo privado para acesso ao contexto de banco de dados de atividade.
+    /// </summary>
     private readonly AtividadeContext _context;
 
     public AtividadeDao(AtividadeContext context)
@@ -12,16 +15,36 @@ public class AtividadeDao
         _context = context;
     }
 
+    /// <summary>
+    /// Método para recuperar todas as atividades cadastradas no banco de dados.
+    /// </summary>
+    /// <returns>
+    /// Retorna uma lista de atividades encontradas no banco de dados.
+    /// </returns>
     public async Task<IEnumerable<Atividade>> GetAtividades()
     {
         return await _context.Atividades.ToListAsync();
     }
 
+    /// <summary>
+    /// Método para recuperar uma atividade específica, a partir do seu ID.
+    /// </summary>
+    /// <param name="id">Identificador único da atividade</param>
+    /// <returns>
+    /// Lista de atividades encontradas no banco de dados.
+    /// </returns>
     public async Task<Atividade> GetAtividadeById(int id)
     {
         return await _context.Atividades.FirstOrDefaultAsync(atividade => atividade.Id == id);
     }
 
+    /// <summary>
+    /// Método para criar uma atividade no banco de dados.
+    /// </summary>
+    /// <param name="atividade">Objeto da classe Atividade</param>
+    /// <returns>
+    /// Retorna as informações da atividade criada.
+    /// </returns>
     public async Task<Atividade> AddAtividade(Atividade atividade)
     {
         _context.Atividades.Add(atividade);
@@ -29,6 +52,13 @@ public class AtividadeDao
         return atividade;
     }
 
+    /// <summary>
+    /// Método para atualizar uma atividade no banco de dados.
+    /// </summary>
+    /// <param name="atividade">Objeto da classe Atividade</param>
+    /// <returns>
+    /// Retorna as informações da atividade atualizada.
+    /// </returns>
     public async Task<Atividade> AtualizaAtividade(Atividade atividade)
     {
         _context.Atividades.Update(atividade);
@@ -36,6 +66,13 @@ public class AtividadeDao
         return atividade;
     }
 
+    /// <summary>
+    /// Método para deletar uma atividade no banco de dados.
+    /// </summary>
+    /// <param name="id">Identificar único de atividade</param>
+    /// <returns>
+    /// Retorna as informações da atividade deletada.
+    /// </returns>
     public async Task<Atividade> DeletaAtividade(int id)
     {
         var atividade = await GetAtividadeById(id);
