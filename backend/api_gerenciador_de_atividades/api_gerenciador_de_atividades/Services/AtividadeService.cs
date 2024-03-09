fed2533bc93938fa224a196a15a063e7f59af496
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2024-03-09 20:04:39 -0300

    feature: adiciona Service da classe de atividade
    O service é responsável por realizar toda a lógica de negócio da API

---
+++ backend/api_gerenciador_de_atividades/api_gerenciador_de_atividades/Services/AtividadeService.cs
@@ -0,0 +1,39 @@
+﻿using api_gerenciador_de_atividades.Data.Dao;
+using api_gerenciador_de_atividades.Data.Dtos;
+using api_gerenciador_de_atividades.Models;
+
+namespace api_gerenciador_de_atividades.Services;
+
+public class AtividadeService
+{
+    private readonly AtividadeDao _atividadeDao;
+
+    public AtividadeService(AtividadeDao atividadeDao)
+    {
+        _atividadeDao = atividadeDao;
+    }
+
+    public async Task<IEnumerable<LeituraAtividadeDto>> GetAtividades()
+    {
+        var atividades = await _atividadeDao.GetAtividades();
+        return atividades.Select(atividade => new LeituraAtividadeDto(atividade.Nome, atividade.Descricao, atividade.Concluida));
+    }
+
+    public async Task<LeituraAtividadeDto> GetAtividadeById(int id)
+    {
+        var atividade = await _atividadeDao.GetAtividadeById(id);
+        return new LeituraAtividadeDto(atividade.Nome, atividade.Descricao, atividade.Concluida);
+    }
+
+    public async Task<LeituraAtividadeDto> AddAtividade(CriarAtividadeDto atividadeDto)
+    {
+        var atividade = new Atividade
+        {
+            Nome = atividadeDto.Nome,
+            Descricao = atividadeDto.Descricao,
+            Concluida = atividadeDto.Concluida
+        };
+        await _atividadeDao.AddAtividade(atividade);
+        return new LeituraAtividadeDto(atividade.Nome, atividade.Descricao, atividade.Concluida);
+    }
+}
